be2dfe765dc330ffe551f6cf5b344f08c436c377
Leonardo <Mac@leonardos-mbp-2.home>
2024-05-01 14:44:01 -0400

    creation of first JS app, Passenger counter

--- 1. Build a Passenger Counter App/02. Let's build a passenger counter app/index.js
+++ 1. Build a Passenger Counter App/02. Let's build a passenger counter app/index.js
@@ -1,17 +1,26 @@
-let count = 0
-let saveEl = document.getElementById("save-el")
-let countEl = document.getElementById("count-el")
+// initialize the count as 0
+let count = 0;
+let countEl = document.getElementById('count-el');
+let saveEl = document.getElementById('save-el');
 
+//listen for clicks on the increment button
+//increment the count variable when the button is clicked
+//change the count-el in the HTML to reflect the new count
 function increment() {
-    count += 1
-    countEl.textContent = count
+    count += 1;
+    countEl.innerText = count;
+    console.log(count);
 }
 
+// Create a function save() which logs out the count when it's called
+// Create a variable that contains both the count and the dash separator
+// Render the variable in the saveEl using innerText
+//NB: Make sure to not delete the existing content of the paragraph
+// Set the count to 0
 function save() {
-    let countStr = count + " - "
-    saveEl.textContenttStr
-    countEl.textContent = 0
+    let setCount = count + ' - '; 
+    saveEl.textContent += setCount;
     count = 0
+    countEl.innerText = count;
 }
 
-console.log("Let's count people on the subway!")
