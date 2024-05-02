a36bcb09bf3ea8537f010a503a2bae05530553a5
Leonardo <Mac@leonardos-mbp-2.home>
2024-05-01 23:14:33 -0400

    added solution code to the practice - part1

--- 2. Practice time - part 1/02. Concatenate two strings in a function/index.js
+++ 2. Practice time - part 1/02. Concatenate two strings in a function/index.js
@@ -3,3 +3,7 @@ let greeting = "Hi there"
 
 // Create a function that logs out "Hi there, Linda!" when called
 
+function greetings() {
+    console.log(greeting + ", " + name + "!");
+}
+greetings();
